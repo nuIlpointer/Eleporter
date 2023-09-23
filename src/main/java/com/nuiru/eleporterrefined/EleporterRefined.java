cddfc83b3ac00b181534a0838155c8d05d68d11b
nuIlpointer <npointa@gmail.com>
2023-09-24 07:19:13 +0900

    とりあえず作り直しの準備

---
+++ src/main/java/com/nuiru/eleporterrefined/EleporterRefined.java
@@ -0,0 +1,17 @@
+package com.nuiru.eleporterrefined;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EleporterRefined extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
